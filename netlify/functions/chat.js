a04cb3efbf3d62294b8487c7a3b8c3fab5cab314
oyeleyeoyebisi7-max <oyeleyeoyebisi7@gmail.com>
2026-05-15 22:40:08 +0100

    Update chat.js

--- netlify/functions/chat.js
+++ netlify/functions/chat.js
@@ -13,7 +13,7 @@ exports.handler = async function(event) {
         "Content-Type": "application/json"
       },
       body: JSON.stringify({
-        model: "meta-llama/llama-3-8b-instruct:free",
+        model: "mistralai/mistral-7b-instruct",
         messages: [
           {
             role: "user",
